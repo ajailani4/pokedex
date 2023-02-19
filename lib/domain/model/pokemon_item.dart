079c753abca9c3ef202aa76219557d057fbf0d76
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-02-19 19:43:21 +0700

    Update PokemonItem

--- lib/domain/model/pokemon_item.dart
+++ lib/domain/model/pokemon_item.dart
@@ -1,6 +1,11 @@
 class PokemonItem {
   final String id;
   final String name;
+  final String image;
 
-  const PokemonItem({required this.id, required this.name});
+  const PokemonItem({
+    required this.id,
+    required this.name,
+    required this.image
+  });
 }
