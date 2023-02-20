e9e32248c089d5d7bd2b5b3fbcadd36cbf95987d
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-02-20 14:55:37 +0700

    Add Injector to implement dependency injection

---
+++ lib/di/injector.dart
@@ -0,0 +1,15 @@
+import 'package:pokedex/data/repository/pokemon_repository_impl.dart';
+import 'package:pokedex/domain/repository/pokemon_repository.dart';
+import 'package:pokedex/domain/use_case/get_pokemon_list_use_case.dart';
+
+class Injector {
+  Injector._();
+
+  static final instance = Injector._();
+
+  // Repository
+  PokemonRepository get pokemonRepository => PokemonRepositoryImpl();
+
+  // Use Case
+  GetPokemonListUseCase get getPokemonListUseCase => GetPokemonListUseCase(pokemonRepository: pokemonRepository);
+}
--- lib/domain/use_case/get_pokemon_list_use_case.dart
+++ lib/domain/use_case/get_pokemon_list_use_case.dart
@@ -1,10 +1,11 @@
 import 'package:pokedex/domain/repository/pokemon_repository.dart';
 
+import '../../di/injector.dart';
+
 class GetPokemonListUseCase {
   final PokemonRepository pokemonRepository;
 
-  GetPokemonListUseCase({required PokemonRepository pokemonRepository}) 
-      : pokemonRepository = Injector.instance.pokemonRepository;
+  GetPokemonListUseCase({required this.pokemonRepository});
   
   call() {
     return pokemonRepository.getPokemonList();
